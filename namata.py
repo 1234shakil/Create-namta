28f3eca153d9ad9e3f6c9a08f0398f9358471961
Yasin Arafat Shakil <yashakil290905@gamil.com>
2024-03-19 16:41:15 +0600

    namata

---
+++ namata.py
@@ -0,0 +1,14 @@
+'''
+b = int(input("Enter a Number :"))
+a = 1
+while a <= 10 :
+    x = b ** a
+    print( f"{b} poware of number {a} = {x}")
+    a += 1
+'''
+b = int(input("Enter a Number :"))
+a = 1
+while a <= 10 :
+    x = b * a
+    print( f"{b} * {a} = {x}")
+    a += 1
